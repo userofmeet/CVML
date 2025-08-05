c153d37e216c1ed787f3c4e6c09ec746a1a74c6b
Meet Jain <meetjain538@gmail.com>
2025-08-05 09:26:33 +0530

    Update bit_plane_slicing.py

--- contrast_enhancement/bit_plane_slicing.py
+++ contrast_enhancement/bit_plane_slicing.py
@@ -1,1 +1,16 @@
-
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+image_path = r"C:\MEET\VS CODE\image_processing\bit_plane_slicing.jpg"
+grayscale_image = cv2.imread(image_path, 0)
+height, width = grayscale_image.shape
+plt.figure(figsize=(12, 8))
+for bit_position in range(8):
+    mask = 1 << bit_position
+    sliced_plane = np.bitwise_and(grayscale_image, mask)
+    visible_plane = np.where(sliced_plane > 0, 255, 0).astype(np.uint8)
+    plt.subplot(2, 4, bit_position + 1)
+    plt.imshow(visible_plane, cmap='gray')
+    plt.title(f'Bit Plane {bit_position}')
+    plt.axis('off')
+plt.show()
