1a9ede426ec32a272df3c3920bc35029ba85997d
Meet Jain <meetjain538@gmail.com>
2025-08-05 09:07:47 +0530

    Create inbuilt_histogram.py

---
+++ contrast_enhancement/inbuilt_histogram.py
@@ -0,0 +1,6 @@
+import cv2
+from matplotlib import pyplot as plt
+img = cv2.imread(r"C:\MEET\VS CODE\image_processing\cv.jpeg",0)
+histr = cv2.calcHist([img],[0],None,[256],[0,256])
+plt.plot(histr)
+plt.show()
