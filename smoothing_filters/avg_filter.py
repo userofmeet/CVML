96c9353af2d704e0297d2e923fd3a00d88965db9
Meet Jain <meetjain538@gmail.com>
2025-08-12 11:30:04 +0530

    Update avg_filter.py

--- smoothing_filters/avg_filter.py
+++ smoothing_filters/avg_filter.py
@@ -2,15 +2,15 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-path = r"D:\LAB SOURCE new\standard_test_images\standard_test_images\house.tif"
+path = r"D:\LAB SOURCE new\standard_test_images\standard_test_images\lena_gray_256.tif"
 img = cv2.imread(path, 0)
 OP = []
 title = ['filter_1','filter_2','filter_3']
 
 for i in range (3):
-    j = i + 4
+    j = i
     k = np.array(np.ones((3+j, 3+j), np.float32))/(3+j)**2 
-    
+    j = j + 4
     output = cv2.filter2D(img, -1, k)
     plt.subplot(1,3,i+1)
     plt.imshow(output,cmap='gray')
