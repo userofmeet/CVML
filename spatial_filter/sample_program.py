7957a0a222c3fa0edfec5216af1ad066c1511b9e
Meet Jain <meetjain538@gmail.com>
2025-10-14 10:20:25 +0530

    Create sample_program.py

---
+++ spatial_filter/sample_program.py
@@ -0,0 +1,18 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+imgpath = r"C:\MEET\B-Tech\sem_7\image_processing\cv.jpg"
+img = cv2.imread(imgpath, 0)  
+k1 = np.array([[-1, -1, -1],
+               [-1,  9, -1],
+               [-1, -1, -1]])
+print(k1)
+output = cv2.filter2D(img, -1, k1)
+plt.subplot(1, 2, 1)
+plt.imshow(img, cmap='gray')
+plt.title('Original Image')
+plt.subplot(1, 2, 2)
+plt.imshow(output, cmap='gray')
+plt.title('Sharpened Image')
+plt.show()
